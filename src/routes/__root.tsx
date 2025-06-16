3f3146c29f9551c9b859ce980ed24b04c50fe323
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2025-06-16 11:49:35 +0200

    feature/appbar - implemented appbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,33 @@
-import { createRootRoute, Outlet, useMatchRoute } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  Outlet,
+  useMatchRoute,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Box, CssBaseline } from "@mui/material";
 import { Appbar } from "../components/appbar";
+import type { ReactElement } from "react";
+import type { RouterContext } from "../main";
 
-export const Route = createRootRoute({
-  component: () => {
-    const matchRoute = useMatchRoute();
-    const isAuthRoute =
-      matchRoute({ to: "/login" }) || matchRoute({ to: "/register" });
-
-    return (
-      <>
-        <CssBaseline />
-        {!isAuthRoute && (
-          <Box sx={{ flexGrow: 1 }}>
-            <Appbar />
-          </Box>
-        )}
-        <Outlet />
-        <TanStackRouterDevtools />
-      </>
-    );
-  },
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: RootLayout,
 });
+
+function RootLayout(): ReactElement {
+  const matchRoute = useMatchRoute();
+  const isAuthRoute =
+    matchRoute({ to: "/login" }) || matchRoute({ to: "/register" });
+
+  return (
+    <>
+      <CssBaseline />
+      {!isAuthRoute && (
+        <Box sx={{ flexGrow: 1 }}>
+          <Appbar />
+        </Box>
+      )}
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
